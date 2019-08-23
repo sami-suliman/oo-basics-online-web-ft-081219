6b416d07c6054214acd3dc3ff620d574cd313a57
sami suliman <sami.bco@gmail.com>
2019-08-23 02:24:35 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,13 @@
 # Make your shoe class here!
 
 class Shoe 
+  attr_accessor :color 
+  
   def initialize(brand)
     @brand = brand
   end
   def brand 
     @brand
   end 
-
-  def color 
-    @color
-  end 
+  
 end 
